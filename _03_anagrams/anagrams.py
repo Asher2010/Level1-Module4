3b6440bcba3a2ceaed14904b78199ab04aac244f
league <asherwelsbie@icloud.com>
2024-07-24 11:55:34 -0700

    middle of anagrams

--- _03_anagrams/anagrams.py
+++ _03_anagrams/anagrams.py
@@ -7,8 +7,6 @@ import tkinter as tk
 # TODO Use this dictionary of anagrams to create an anagrams GUI word game.
 #  Look at 'anagrams_game_example.png' in this folder for an example.
 
-
-
 word_anagrams = {
     "action": ["cation"],
     "agree": ["eager"],
@@ -48,6 +46,7 @@ word_anagrams = {
     "wonderful": ["underflow"],
     "zeal": ["laze"]}
 
+
 class Anagrams(tk.Tk):
 
     def __init__(self):
@@ -66,12 +65,6 @@ class Anagrams(tk.Tk):
 
         self.input_entry.bind('<KeyRelease>', self.on_key_release)
 
-        for key, value in word_anagrams.items():
-            if self.label3 == value:
-                print("correct")
-
-
-
         self.button = tk.Button(self, text='Get New Word!', fg='black', font=('courier new', 16, 'bold'))
 
         self.button.place(relx=0.73, rely=0.1, relwidth=0.25, relheight=0.1)
@@ -84,33 +77,37 @@ class Anagrams(tk.Tk):
 
         self.button2.bind('<ButtonPress>', self.on_button2_press)
 
-        self.label = tk.Label(self, text="Guess the anagram for the word: " + key, fg='black', font=('courier new', 16, 'bold'))
+        self.label = tk.Label(self, text="Guess the anagram for the word:", fg='black', font=('courier new', 16, 'bold'))
 
         self.label.place(relx=.001, rely=.1, relwidth=.5, relheight=.1)
+
+        self.label4 = tk.Label(self, text='test', fg='black', font=('courier new', 16, 'bold'))
+
+        self.label4.place(relx=.51, rely=.1, relwidth=.13, relheight=.1)
+
+        self.label4.bind('<ButtonPress>', self.on_button_press)
 
         self.label2 = tk.Label(self, text="Guesses remaining: " + str(self.guesses), fg='black', font=('courier new', 16, 'bold'))
 
         self.label2.place(relx=.001, rely=.4, relwidth=.38, relheight=.1)
 
-        self.label3 = tk.Label(self, text="", fg='black', font=('courier new', 16, 'bold'))
-
-        self.label3.place(relx=.3, rely=.8, relwidth=.5, relheight=.1)
-
     def on_button_press(self, event):
-        print("button pressed")
-
+        self.label4.configure(text=str(word_anagrams.keys()))
 
     def on_button2_press(self, event):
-        print("button 2 pressed")
-        self.guesses -= 1
-        #self.guesses_remaining_label.congfigure()
-
-
+        for key, value in word_anagrams.items():
+            val = word_anagrams[key]
+            if self.label3 == val:
+                print("correct")
+            else:
+                self.guesses -= 1
+                self.label2.configure(text="Guesses remaining: " + str(self.guesses))
+                if self.guesses == 0:
+                    exit()
 
     def on_key_release(self, event):
         label_text = self.input_entry.get()
         self.label3.config(text=label_text)
-        print("key pressed")
 
 
 if __name__ == '__main__':
